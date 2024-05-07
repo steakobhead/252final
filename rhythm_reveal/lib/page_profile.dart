10c8d130b0fe668efc8e586aac3aaf5cb8efeb19
jubskiko <93483786+jubskiko@users.noreply.github.com>
2024-05-07 10:11:09 -0400

    mess

--- rhythm_reveal/lib/page_profile.dart
+++ rhythm_reveal/lib/page_profile.dart
@@ -51,67 +51,70 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget buildUserProfilePage(UserProfile profile) {
-  return Scaffold(
-    body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        profile.username,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          profile.username,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 8),
-                      const CircleAvatar(
-                        radius: 50,
-                        backgroundImage:
-                            AssetImage('assets/profile_placeholder.png'),
-                      ),
-                      const SizedBox(height: 16),
-                    ],
+                        const SizedBox(height: 8),
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage('assets/profile_placeholder.png'),
+                        ),
+                        const SizedBox(height: 16),
+                        const SizedBox(height: 16),
+                        YesterdayBumpSection(fullHistory: profile.fullHistory),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: FavoriteSongsSection(favoriteSongs: profile.topThree),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Container(
-              height: 300,
-              child: FullHistorySection(
-                fullHistory: profile.fullHistory,
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child:
+                        FavoriteSongsSection(favoriteSongs: profile.topThree),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FullHistoryPage()),
-                );
-              },
-              child: const Text('View Full History'),
-            ),
-          ],
+              Container(
+                height: 300,
+                child: FullHistorySection(
+                  fullHistory: profile.fullHistory,
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FullHistoryPage()),
+                  );
+                },
+                child: const Text('View Full History'),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
 
 class FavoriteSongsSection extends StatelessWidget {
@@ -160,10 +163,117 @@ class FullHistorySection extends StatelessWidget {
       itemCount: fullHistory.length,
       itemBuilder: (context, index) {
         final history = fullHistory[index];
-        return ListTile(
-          title: Text('${history.songName} by ${history.artist}'),
+        return Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+          elevation: 4, // Adds shadow effect
+          child: ListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            title: Text(
+              history.songName,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            subtitle: Text(
+              history.artist,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+          ),
         );
       },
     );
+  }
+}
+
+class YesterdayBumpSection extends StatelessWidget {
+  final List<models.SongHistory> fullHistory;
+
+  const YesterdayBumpSection({Key? key, required this.fullHistory})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (fullHistory.isNotEmpty) {
+      models.SongHistory lastSong = fullHistory.first;
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Yesterday's Bump",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue[50],
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 6,
+                  offset: const Offset(0, 3),
+                )
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  lastSong.songName,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  lastSong.artist,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+                const SizedBox(height: 8),
+              ],
+            ),
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            "Yesterday's Bump",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            "No songs played yesterday.",
+            style: TextStyle(fontSize: 16, color: Colors.black54),
+          ),
+        ],
+      );
+    }
   }
 }
